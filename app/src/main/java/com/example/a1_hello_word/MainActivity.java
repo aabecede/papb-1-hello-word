68d82a1cadaf589f198ea36fef9a01efb24e8da2
aabecede <aabdul.jabbar1301@gmail.com>
2023-02-19 11:49:29 +0700

    [LAYOUT] Update Home

--- app/src/main/java/com/example/a1_hello_word/MainActivity.java
+++ app/src/main/java/com/example/a1_hello_word/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a1_hello_word;
 
 import android.os.Bundle;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,11 +19,15 @@ import com.example.a1_hello_word.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    TextView textview_first;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +35,30 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        textview_first = findViewById(R.id.textview_first);
+        button = findViewById(R.id.click_me_animate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//                YoYo.with(Techniques.Bounce).duration(1000).repeat(1).playOn(textview_first);
+                YoYo.with(Techniques.Tada)
+                        .duration(700)
+                        .repeat(5)
+                        .playOn(findViewById(R.id.textview_first));
+            }
+        });
+
+//        YoYo.YoYoString yoYoString = YoYo.with(Techniques.SlideInRight).duration(1500).repeat(2).playOn(textView);
+
+
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
